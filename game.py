bc7090ed5d6ce9f8ca53ef74371251216df02831
algoccs <algodemoacc@gmail.com>
2025-06-04 17:40:29 -0400

    Update game.py
    Creacion de clase Player, implementacion de mecanica de movimiento

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ init()
 
 SW, SH = 640, 480
 FPS = 120
+PLAYER_IMG = 'paddle.png'
 
 screen = display.set_mode((SW, SH))
 display.set_caption('Pong')
@@ -21,6 +22,24 @@ class Character(sprite.Sprite):
     def reset(self):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(Character):
+    def update_p1(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y >= 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y <= SH - self.height:
+            self.rect.y += self.speed
+
+    def update_p2(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y >= 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y <= SH - self.height:
+            self.rect.y += self.speed
+
+player_1 = Player('paddle.png', 0, 100, 40, 100, 5)
+player_2 = Player('paddle2.png', SW -40, 100, 40, 100, 5)
+
 clock = time.Clock()
 run = True
 finish = False
@@ -28,6 +47,12 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
+
+    screen.fill((100, 40, 150))
+    player_1.reset()
+    player_1.update_p1()
+    player_2.reset()
+    player_2.update_p2()
 
     display.update()
     clock.tick(FPS)
